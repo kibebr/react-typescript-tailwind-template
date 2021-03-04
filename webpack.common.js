3a82bab8325161438f8a518cd4ceb629175878d1
kibebr <0763722@students.dadeschools.net>
2021-03-04 12:22:53 +0000

    added support for dotenv and importing svg as react components

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
 module.exports = {
   entry: './src/index.tsx',
   resolve: {
@@ -7,6 +10,23 @@ module.exports = {
     rules: [{
       test: /\.(ts|tsx)$/,
       loader: 'ts-loader'
+    }, {
+      test: /\.svg$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          ref: true,
+          icon: true
+        }
+      }, {
+        loader: 'url-loader'
+      }]
     }]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed)
+    })
+  ]
 }
